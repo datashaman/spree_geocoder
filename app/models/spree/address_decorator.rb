6f7ed1dda29ed38af1e71794b8a5c653bba81607
datashaman <marlinf@datashaman.com>
2015-05-08 20:16:05 +0200

    Geocoding working on address and stock_location models

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -1,12 +1,12 @@
 require 'geocoder'
 
-Spree::StockLocation.class_eval do
+Spree::Address.class_eval do
     extend Geocoder::Model::ActiveRecord
 
     geocoded_by :address
     after_validation :geocode
 
     def address
-        [ address1, address2, city, state_text, country.name ].join(', ')
+        [ address1, address2, city, state_text, country.name ].compact.join(', ')
     end
 end
